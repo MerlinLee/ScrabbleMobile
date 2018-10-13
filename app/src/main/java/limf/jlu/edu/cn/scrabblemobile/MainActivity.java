773d9715fa2f715acd58177e3b9083b19285b4ee
merlin <merlinlee5513@gmail.com>
2018-10-14 03:30:28 +1100

    ADD PROTOCOL

--- app/src/main/java/limf/jlu/edu/cn/scrabblemobile/MainActivity.java
+++ app/src/main/java/limf/jlu/edu/cn/scrabblemobile/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.alibaba.fastjson.JSON;
 import com.vilyever.socketclient.SocketClient;
@@ -21,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText eT_ip;
     private EditText eT_port;
     private Button btn_login;
+    private TextView textView_username;
+    private TextView textView_ip;
+    private TextView textView_port;
     private MyBtnClicker myBtnClicker = new MyBtnClicker();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
         eT_username = findViewById(R.id.editText_username);
         eT_ip = findViewById(R.id.editText_ip);
         eT_port = findViewById(R.id.editText_port);
+        textView_username = findViewById(R.id.textView_username);
+        textView_ip = findViewById(R.id.textView_ip);
+        textView_port = findViewById(R.id.textView_port);
         btn_login = findViewById(R.id.btn_login);
         btn_login.setOnClickListener(myBtnClicker);
     }
